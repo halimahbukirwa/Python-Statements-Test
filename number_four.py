7c0d087c4e4a749cf044bb52b9a1a7546e9a549f
Halimah Bukirwa <halimahbukirwa24@gmail.com>
2021-06-22 22:57:18 +0300

    program that prints a word whose length is even

---
+++ number_four.py
@@ -0,0 +1,7 @@
+# Go through the string below and if the length of a word is even print "even!"
+
+st = 'Print every word in this sentence that has an even number of letters'
+
+for word in st.split():
+    if len(word) % 2 == 0:
+        print(word)
